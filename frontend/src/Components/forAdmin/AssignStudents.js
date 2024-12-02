350bb56b6fd487a107be270c6ebcda78719a35dd
Skinnyytallboy <m.jalalahmed2004@gmail.com>
2024-12-02 05:54:58 +0500

    feat: complete all dashbaords

--- frontend/src/Components/forAdmin/AssignStudents.js
+++ frontend/src/Components/forAdmin/AssignStudents.js
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { fetchCoursesAdmin, fetchAllStudents , assignStudentsToCourse } from "../../Services/api";
+import { fetchCoursesAdmin, fetchAllStudents, assignStudentsToCourse } from "../../Services/api";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const AssignStudents = () => {
   const [students, setStudents] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const [selectedStudents, setSelectedStudents] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loadingStudents, setLoadingStudents] = useState(true);
+  const [loadingCourses, setLoadingCourses] = useState(true);
+
   useEffect(() => {
     const fetchStudents = async () => {
       const data = await fetchAllStudents();
       setStudents(data.students);
+      setLoadingStudents(false);
     };
     fetchStudents();
   }, []);
 
-  const [courses, setCourses] = useState([]);
   useEffect(() => {
     const fetchCourses = async () => {
       const data = await fetchCoursesAdmin();
       setCourses(data.courses);
+      setLoadingCourses(false);
     };
     fetchCourses();
   }, []);
-
-  const [selectedStudents, setSelectedStudents] = useState([]);
-  const [selectedCourse, setSelectedCourse] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
 
   const handleStudentSelection = (studentId) => {
     setSelectedStudents((prevSelected) => {
@@ -53,8 +58,6 @@ const AssignStudents = () => {
     student.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  console.log(selectedStudents, selectedCourse);
-
   return (
     <div className="p-2 bg-white min-h-screen space-y-8">
       <h3 className="text-3xl font-bold text-indigo-800 text-center">
@@ -78,37 +81,52 @@ const AssignStudents = () => {
           <div className="space-y-2">
             <p className="text-indigo-800 font-medium">Select Students:</p>
             <div className="grid grid-cols-2 gap-4 max-h-40 overflow-y-auto">
-              {filteredStudents.map((student) => (
-                <div key={student.student_id} className="flex items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    id={`student-${student.student_id}`}
-                    checked={selectedStudents.includes(student.student_id)}
-                    onChange={() => handleStudentSelection(student.student_id)}
-                    className="w-4 h-4 text-blue-500 focus:ring-blue-400 border-indigo-300 rounded"
-                  />
-                  <label
-                    htmlFor={`student-${student.student_id}`}
-                    className="text-gray-700"
-                  >
-                    {student.name}
-                  </label>
+              {loadingStudents ? (
+                <div className="flex justify-center items-center w-full h-full">
+                  <ClipLoader size={50} color={"#4c6ef5"} />
                 </div>
-              ))}
+              ) : (
+                filteredStudents.map((student) => (
+                  <div key={student.student_id} className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      id={`student-${student.student_id}`}
+                      checked={selectedStudents.includes(student.student_id)}
+                      onChange={() => handleStudentSelection(student.student_id)}
+                      className="w-4 h-4 text-blue-500 focus:ring-blue-400 border-indigo-300 rounded"
+                    />
+                    <label
+                      htmlFor={`student-${student.student_id}`}
+                      className="text-gray-700"
+                    >
+                      {student.name}
+                    </label>
+                  </div>
+                ))
+              )}
             </div>
           </div>
-          <select
-            value={selectedCourse}
-            onChange={(e) => setSelectedCourse(e.target.value)}
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <option value="">Select Course</option>
-            {courses.map((course) => (
-              <option key={course.course_id} value={course.course_id}>
-                {course.course_name}
-              </option>
-            ))}
-          </select>
+          <div className="space-y-2">
+            <p className="text-indigo-800 font-medium">Select Course:</p>
+            {loadingCourses ? (
+              <div className="flex justify-center items-center w-full h-full">
+                <ClipLoader size={50} color={"#4c6ef5"} />
+              </div>
+            ) : (
+              <select
+                value={selectedCourse}
+                onChange={(e) => setSelectedCourse(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                <option value="">Select Course</option>
+                {courses.map((course) => (
+                  <option key={course.course_id} value={course.course_id}>
+                    {course.course_name}
+                  </option>
+                ))}
+              </select>
+            )}
+          </div>
           <button
             onClick={handleAssignStudents}
             className="w-full bg-indigo-500 text-white px-3 py-2 rounded-lg hover:bg-indigo-600 transition duration-300 mt-4"
